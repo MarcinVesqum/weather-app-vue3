e4a698e7cae7fa890e45977a604349714bbdbe7b
Vesqum <marcin.pelszyk30@gmail.com>
2022-09-14 09:37:43 +0200

    added Navigation and first configuration

---
+++ weater-app/src/components/BaseModal.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="absolute flex flex-col">
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
---
+++ weater-app/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#06113C",
+        "weather-secondary": "#FF8C32"
+      }
+    },
+    fontFamily: {
+      Montserrat: ["Montserrat, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    }
+  },
+  plugins: [],
+}
